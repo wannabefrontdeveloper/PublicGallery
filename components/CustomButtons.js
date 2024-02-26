6eb6d64dfcb6e7a44a2fb053d3f1653700aaac75
wannabefrontdeveloper <giwonk@naver.com>
2024-02-26 11:04:47 +0900

    코드생성 임의의 스타일을 가진 컴포넌트입니다.

---
+++ components/CustomButtons.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {StyleSheet, View, Pressable, Text, Platform} from 'react-native';
+
+function CustomButton({onPress, title, hasMarginBottom}) {
+  return (
+    <View
+      style={[styles.block, styles.overflow, hasMarginBottom && styles.margin]}>
+      <Pressable
+        onPress={onPress}
+        styles={({pressed}) => [
+          styles.wrapper,
+          Platform.OS === 'ios' && pressed && {opacity: 0.5},
+        ]}
+        android_ripple={{
+          color: '#ffffff',
+        }}>
+        <Text style={[styles.text]}>{title}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  overflow: {
+    borderRadius: 4,
+    overflow: 'hidden',
+  },
+  wrapper: {
+    borderRadius: 4,
+    height: 48,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#6200ee',
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 14,
+    color: 'white',
+  },
+  margin: {
+    marginBottom: 8,
+  },
+});
+
+export default CustomButton;
