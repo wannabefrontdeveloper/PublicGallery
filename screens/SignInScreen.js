a79e31c145859c59d5b36eaf5a0af229ff334e40
wannabefrontdeveloper <giwonk@naver.com>
2024-02-27 20:54:28 +0900

    코드수정 BorderedInput을 사용할 때 placeholder Props를 설정하였습니다.

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -9,8 +9,8 @@ function SignInScreen() {
     <SafeAreaView style={styles.fullscreen}>
       <Text style={styles.text}>PublicGallery</Text>
       <View style={styles.form}>
-        <BorderedInput hasMarginBottom />
-        <BorderedInput />
+        <BorderedInput hasMarginBottom placeholder="이메일" />
+        <BorderedInput placeholder="비밀번호" />
         <View style={styles.buttons}>
           <CustomButton title="로그인" hasMarginBottom />
           <CustomButton title="회원가입" />
