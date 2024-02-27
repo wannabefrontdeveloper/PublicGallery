d516f5a9b33a5d85971369ec3672a3cff3b86823
wannabefrontdeveloper <giwonk@naver.com>
2024-02-27 22:29:33 +0900

    코드수정 loading 값이 true일 때 스피너를 보여주도록 하였습니다.

--- components/SignButtons.js
+++ components/SignButtons.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {ActivityIndicator, StyleSheet, View} from 'react-native';
 import CustomButton from './CustomButton';
 import {useNavigation} from '@react-navigation/native';
 
-function SignButtons({isSignUp, onSubmit}) {
+function SignButtons({isSignUp, onSubmit, loading}) {
   const navigation = useNavigation();
 
   const primaryTitle = isSignUp ? '회원가입' : '로그인';
@@ -17,9 +17,17 @@ function SignButtons({isSignUp, onSubmit}) {
     }
   };
 
+  if (loading) {
+    return (
+      <View style={styles.spinnerWrapper}>
+        <ActivityIndicator size={32} color="#6200ee" />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.buttons}>
-      <CustomButton ttile={primaryTitle} hasMarginBottom onPress={onSubmit} />
+      <CustomButton title={primaryTitle} hasMarginBottom onPress={onSubmit} />
       <CustomButton
         title={secondaryTitle}
         theme="secondary"
@@ -30,6 +38,12 @@ function SignButtons({isSignUp, onSubmit}) {
 }
 
 const styles = StyleSheet.create({
+  spinnerWrapper: {
+    marginTop: 64,
+    height: 104,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   buttons: {
     marginTop: 64,
   },
