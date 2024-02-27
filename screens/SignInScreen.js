02ca46b61ef3f52d9e3c80ddbc3a915889a61b86
wannabefrontdeveloper <giwonk@naver.com>
2024-02-27 21:21:12 +0900

    코드수정 하나의 useState를 사용해 객체 형태의 상태를 사용하도록 하였습니다.

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,51 +1,101 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import BorderedInput from '../components/BorderedInput';
 import CustomButton from '../components/CustomButton';
 
 function SignInScreen({navigation, route}) {
   const {isSignUp} = route.params ?? {};
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const createChangeTextHandler = name => value => {
+    setForm({...form, [name]: value});
+  };
+  const onSubmit = () => {
+    Keyboard.dismiss();
+    console.log(form);
+  };
   return (
-    <SafeAreaView style={styles.fullscreen}>
-      <Text style={styles.text}>PublicGallery</Text>
-      <View style={styles.form}>
-        <BorderedInput hasMarginBottom placeholder="이메일" />
-        <BorderedInput placeholder="비밀번호" hasMarginBottom={isSignUp} />
-        {isSignUp && <BorderedInput placeholder="비밀번호 확인" />}
-        <View style={styles.buttons}>
-          {isSignUp ? (
-            <>
-              <CustomButton title="회원가입" hasMarginBottom />
-              <CustomButton
-                title="로그인"
-                theme="secondary"
-                onPress={() => {
-                  navigation.goBack();
-                }}
-              />
-            </>
-          ) : (
-            <>
-              <CustomButton title="로그인" hasMarginBottom />
-              <CustomButton
-                title="회원가입"
-                theme="secondary"
-                onPress={() => {
-                  navigation.push('SignIn', {
-                    isSignUp: true,
-                  });
-                }}
-              />
-            </>
+    <KeyboardAvoidingView
+      style={styles.KeyboardAvoidingView}
+      behavior={Platform.select({ios: 'padding'})}>
+      <SafeAreaView style={styles.fullscreen}>
+        <Text style={styles.text}>PublicGallery</Text>
+        <View style={styles.form}>
+          <BorderedInput
+            hasMarginBottom
+            placeholder="이메일"
+            value={form.email}
+            onChangeText={createChangeTextHandler('email')}
+          />
+          <BorderedInput
+            placeholder="비밀번호"
+            hasMarginBottom={isSignUp}
+            value={form.password}
+            onChangeText={createChangeTextHandler('password')}
+          />
+          {isSignUp && (
+            <BorderedInput
+              placeholder="비밀번호 확인"
+              value={form.confirmPassword}
+              onChangeText={createChangeTextHandler('confirmPassword')}
+            />
           )}
+          <View style={styles.buttons}>
+            {isSignUp ? (
+              <>
+                <CustomButton
+                  title="회원가입"
+                  hasMarginBottom
+                  onPress={onSubmit}
+                />
+                <CustomButton
+                  title="로그인"
+                  theme="secondary"
+                  onPress={() => {
+                    navigation.goBack();
+                  }}
+                />
+              </>
+            ) : (
+              <>
+                <CustomButton
+                  title="로그인"
+                  hasMarginBottom
+                  onPress={onSubmit}
+                />
+                <CustomButton
+                  title="회원가입"
+                  theme="secondary"
+                  onPress={() => {
+                    navigation.push('SignIn', {
+                      isSignUp: true,
+                    });
+                  }}
+                />
+              </>
+            )}
+          </View>
         </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
+  keyboardAvoidingView: {
+    flex: 1,
+  },
   fullscreen: {
     flex: 1,
     alignItems: 'center',
