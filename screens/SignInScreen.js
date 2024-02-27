459373df63a18a97d81e2412cf9dd4630f5c6fe3
wannabefrontdeveloper <giwonk@naver.com>
2024-02-27 21:26:33 +0900

    코드추가 1. 이메일 인풋에서 이메일을 더 편하게 입력할 수 있도록 첫 번째 문자 자동 대문자, 자동 수정을 비활성화하고, 이메일 자동 완성과 이메일 전용 키도르를 활성화하였습니다. 2. 입력한 비밀번호를 화면에서 숨기도록 하였습니다.

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -37,18 +37,26 @@ function SignInScreen({navigation, route}) {
             placeholder="이메일"
             value={form.email}
             onChangeText={createChangeTextHandler('email')}
+            autoCapitalize="none"
+            autoCorrect={false}
+            autoCompleteType="email"
+            keyboardType="email-address"
           />
           <BorderedInput
             placeholder="비밀번호"
             hasMarginBottom={isSignUp}
             value={form.password}
             onChangeText={createChangeTextHandler('password')}
+            autoCapitalize="none"
+            secureTextEntry
           />
           {isSignUp && (
             <BorderedInput
               placeholder="비밀번호 확인"
               value={form.confirmPassword}
               onChangeText={createChangeTextHandler('confirmPassword')}
+              autoCapitalize="none"
+              secureTextEntry
             />
           )}
           <View style={styles.buttons}>
