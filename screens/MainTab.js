eb8c248b5800b3bdc856addbd6cb40f4a3bd48b9
wannabefrontdeveloper <giwonk@naver.com>
2024-02-28 19:09:17 +0900

    코드수정 HomeStack과 MyProfileStack을 불러와 넣어주었습니다.

--- screens/MainTab.js
+++ screens/MainTab.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomeStack from './HomeStack';
+import MyProfileStack from './MyProfileStack';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,7 +14,22 @@ function MainTab() {
         tabBarShowLabel: false,
         tabBarActiveTintColor: '#6200ee',
       }}>
-      {/* Todo 화면 추가 */}
+      <Tab.Screen
+        name="HomeStack"
+        component={HomeStack}
+        options={{
+          tabBarIcon: ({color}) => <Icon name="home" size={24} color={color} />,
+        }}
+      />
+      <Tab.Screen
+        name="MyProfileStack"
+        component={MyProfileStack}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Icon name="person" size={24} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
