2a4fa0f195a4ef127f16951d6afde881c2af2dd9
wannabefrontdeveloper <giwonk@naver.com>
2024-02-28 22:17:18 +0900

    코드수정 FeedScreen 화면이 보일때 포스트 정보들을 띄우도록 구현하였습니다.

--- screens/FeedScreen.js
+++ screens/FeedScreen.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {FlatList} from 'react-native';
+import PostCard from '../components/PostCard';
+import {getPosts} from '../lib/posts';
 
 function FeedScreen() {
-  return <View />;
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    // 컴포넌트가 처음 마운트될 때 포스트 목록을 조회한 후 `posts` 상태에 담기
+    getPosts().then(setPosts);
+  }, []);
+
+  return (
+    <FlatList
+      data={posts}
+      renderItem={renderItem}
+      keyExtractor={item => item.id}
+    />
+  );
 }
+
+const renderItem = ({item}) => (
+  <PostCard
+    createdAt={item.createdAt}
+    description={item.description}
+    id={item.id}
+    user={item.user}
+    photoURL={item.photoURL}
+  />
+);
 
 export default FeedScreen;
