3b0317de44ce76dd9e018b28397bf37beac6489a
wannabefrontdeveloper <giwonk@naver.com>
2024-02-28 22:29:17 +0900

    코드수정 getOlderPosts함수를 사용하도록 하였습니다.

--- screens/FeedScreen.js
+++ screens/FeedScreen.js
@@ -1,21 +1,45 @@
 import React, {useEffect, useState} from 'react';
-import {FlatList} from 'react-native';
+
+import {ActivityIndicator, FlatList, StyleSheet} from 'react-native';
 import PostCard from '../components/PostCard';
-import {getPosts} from '../lib/posts';
+import {getOlderPosts, getPosts, PAGE_SIZE} from '../lib/posts';
 
 function FeedScreen() {
   const [posts, setPosts] = useState(null);
+  // 마지막 포스트까지 조회했음을 명시하는 상태
+
+  const [noMorePost, setNoMorePost] = useState(false);
 
   useEffect(() => {
     // 컴포넌트가 처음 마운트될 때 포스트 목록을 조회한 후 `posts` 상태에 담기
     getPosts().then(setPosts);
   }, []);
 
+  const onLoadMore = async () => {
+    if (noMorePost || !posts || posts.length < PAGE_SIZE) {
+      return;
+    }
+    const lastPost = posts[posts.lenght - 1];
+    const olderPosts = await getOlderPosts(lastPost.id);
+    if (olderPosts.length < PAGE_SIZE) {
+      setNoMorePost(true);
+    }
+    setPosts(posts.concat(olderPosts));
+  };
+
   return (
     <FlatList
       data={posts}
       renderItem={renderItem}
       keyExtractor={item => item.id}
+      contentContainerStyle={styles.container}
+      onEndReached={onLoadMore}
+      onEndReachedThreshold={0.75}
+      ListFooterComponent={
+        !noMorePost && (
+          <ActivityIndicator style={styles.spinner} size={32} color="#6200ee" />
+        )
+      }
     />
   );
 }
@@ -29,5 +53,14 @@ const renderItem = ({item}) => (
     photoURL={item.photoURL}
   />
 );
+
+const styles = StyleSheet.create({
+  container: {
+    paddingBottom: 48,
+  },
+  spinner: {
+    height: 64,
+  },
+});
 
 export default FeedScreen;
