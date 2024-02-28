f9885bc596a2ee3a6484a5e6e840764a2dfd5cbe
wannabefrontdeveloper <giwonk@naver.com>
2024-02-28 21:03:23 +0900

    코드추가 포스트 작성화면에서 res 라우트 파라미터를 받아와 이를 Image 컴포넌트를 사용해 화면에 보여주도록 하였습니다.

--- screens/UploadScreen.js
+++ screens/UploadScreen.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {useRoute} from '@react-navigation/native';
+import React, {useEffect, useRef, useState} from 'react';
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Image,
+  useWindowDimensions,
+} from 'react-native';
 
 function UploadScreen() {
-  return <View style={styles.block} />;
+  const route = useRoute();
+  const {res} = route.params || {};
+  const {width} = useWindowDimensions();
+
+  return (
+    <View style={styles.block}>
+      <Image
+        source={{uri: res.assets[0]?.uri}}
+        style={[styles.image, {height: width}]}
+        resizeMode="cover"
+      />
+      <TextInput
+        style={styles.input}
+        multiline={true}
+        placeholder="이 사진에 대한 설명을 입력하세요..."
+        textAlignVertical="top"
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  block: {},
+  block: {
+    flex: 1,
+  },
+  image: {width: '100%'},
+  input: {
+    paddingHorizontal: 16,
+    paddingTop: 16,
+    paddingBottom: 16,
+    flex: 1,
+    fontSize: 16,
+  },
 });
 
 export default UploadScreen;
