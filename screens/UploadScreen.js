d9e863d928e2549b046053ee6147966ea3989c4d
wannabefrontdeveloper <giwonk@naver.com>
2024-02-28 21:16:58 +0900

    코드추가 키보드 이벤트에 따라 값을 변경하도록 구현하였습니다.

--- screens/UploadScreen.js
+++ screens/UploadScreen.js
@@ -4,6 +4,8 @@ import {
   StyleSheet,
   TextInput,
   View,
+  Animated,
+  Keyboard,
   Image,
   useWindowDimensions,
 } from 'react-native';
@@ -12,12 +14,38 @@ function UploadScreen() {
   const route = useRoute();
   const {res} = route.params || {};
   const {width} = useWindowDimensions();
+  const animation = useRef(new Animated.Value(width)).current;
+  const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
+
+  useEffect(() => {
+    const didShow = Keyboard.addListener('keyboardDidShow', () =>
+      setIsKeyboardOpen(true),
+    );
+
+    const didHide = Keyboard.addListener('keyboardDidHide', () =>
+      setIsKeyboardOpen(false),
+    );
+
+    return () => {
+      didShow.remove();
+      didHide.remove();
+    };
+  }, []);
+
+  useEffect(() => {
+    Animated.timing(animation, {
+      toValue: isKeyboardOpen ? 0 : width,
+      useNativeDriver: false,
+      duration: 150,
+      delay: 100,
+    }).start();
+  }, [isKeyboardOpen, width, animation]);
 
   return (
     <View style={styles.block}>
       <Image
         source={{uri: res.assets[0]?.uri}}
-        style={[styles.image, {height: width}]}
+        style={[styles.image, {height: animation}]}
         resizeMode="cover"
       />
       <TextInput
