7b3ab4ce91e4be7433c04b10b77b21760549cc1a
wannabefrontdeveloper <giwonk@naver.com>
2024-02-29 13:54:50 +0900

    코드수정 컴포넌트를 눌렀을 때 포스트 화면을 띄우도록 구현하였습니다.

--- components/PostGridItem.js
+++ components/PostGridItem.js
@@ -1,11 +1,14 @@
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
 import {StyleSheet, useWindowDimensions, Image, Pressable} from 'react-native';
 
 function PostGridItem({post}) {
   const dimensions = useWindowDimensions();
   const size = (dimensions.width - 3) / 3;
+  const navigation = useNavigation();
+
   const onPress = () => {
-    // TODO: 단일 포스트 조회 화면 띄우기
+    navigation.navigate('Post', {post});
   };
 
   return (
