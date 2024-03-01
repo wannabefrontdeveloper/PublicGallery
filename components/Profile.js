20b4fa6d50891abd174ebc3dfe5e60d0e088281e
wannabefrontdeveloper <giwonk@naver.com>
2024-03-01 09:35:36 +0900

    코드수정 useCallback을 사용해 해당 함수에서 의존하는 값이 바뀔 때만 새로운 함수를 사용하도록 하였습니다.

--- components/Profile.js
+++ components/Profile.js
@@ -12,15 +12,31 @@ import {getUser} from '../lib/users';
 import Avatar from './Avatar';
 import PostGridItem from './PostGridItem';
 import usePosts from '../hooks/usePosts';
+import {useUserContext} from '../contexts/UserContext';
+import events from '../lib/event';
 
 function Profile({userId}) {
   const [user, setUser] = useState(null);
   const {posts, noMorePost, refreshing, onLoadMore, onRefresh} =
     usePosts(userId);
 
+  const {user: me} = useUserContext();
+  const isMyProfile = me.id === userId;
+
   useEffect(() => {
     getUser(userId).then(setUser);
   }, [userId]);
+
+  useEffect(() => {
+    // 자신의 프로필을 보고 있을 때만 새 포스트 작성 후 새로고침합니다.
+    if (!isMyProfile) {
+      return;
+    }
+    events.addListener('refresh', onRefresh);
+    return () => {
+      events.removeListener('refresh', onRefresh);
+    };
+  }, [isMyProfile, onRefresh]);
 
   if (!user || !posts) {
     return (
