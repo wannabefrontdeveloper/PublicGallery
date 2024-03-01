9a793e94c6e53cbdea75c6f58124cbe4c641b62e
wannabefrontdeveloper <giwonk@naver.com>
2024-03-01 10:17:26 +0900

    코드생성 설정화면입니다.

---
+++ screens/SettingScreen.js
@@ -0,0 +1,12 @@
+import React from ' react';
+import {StyleSheet, View} from 'react-native';
+
+function SettingScreen() {
+  return <View style={styles.block} />;
+}
+
+const styles = StyleSheet.create({
+  block: {},
+});
+
+export default SettingScreen;
