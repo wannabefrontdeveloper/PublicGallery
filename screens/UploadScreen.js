6ef3e25efdef10a678878b39a0fb9db4eb4ce323
wannabefrontdeveloper <giwonk@naver.com>
2024-03-01 09:27:43 +0900

    코드수정 포스트를 작성하고나서 refresh 이벤트를 발생시키도록 하였습니다.

--- screens/UploadScreen.js
+++ screens/UploadScreen.js
@@ -15,6 +15,7 @@ import storage from '@react-native-firebase/storage';
 import {useUserContext} from '../contexts/UserContext';
 import {v4} from 'uuid';
 import {createPost} from '../lib/posts';
+import events from '../lib/event';
 
 function UploadScreen() {
   const route = useRoute();
@@ -39,6 +40,7 @@ function UploadScreen() {
     }
     const photoURL = await reference.getDownloadURL();
     await createPost({description, photoURL, user});
+    events.emit('refresh');
     // TODO: 포스트 목록 새로고침
   }, [res, user, description, navigation]);
   const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
